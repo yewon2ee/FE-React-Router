0f7d5caaa88b77a9fa67e40b1ab83f15c787e62c
yewonee <hyewon8685@naver.com>
2025-05-29 00:37:33 +0900

    과제 제출

--- src/components/ColorPage.jsx
+++ src/components/ColorPage.jsx
@@ -1,11 +1,18 @@
 import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 import '../styles/ColorPage.css';
 
 function ColorPage() {
+  const { id } = useParams(); // URL에서 id 가져오기
+
+  useEffect(() => {
+    document.body.style.backgroundColor = id;
+
+  }, [id]);
 
   return (
-    <div className="color-page">
-    배경 페이지입니다 
+    <div id="color-page" className="color-page">
+     컬러 페이지 입니다 .
     </div>
   );
 }
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,14 +1,40 @@
 import { useNavigate } from 'react-router-dom';
 import '../styles/MainPage.css';
+import { useEffect } from 'react';
+
+
+const colors = ['red', 'blue', 'pink', 'orange', 'yellow'];
 
 const Mainpage = () => {
- 
+  const navigate = useNavigate();
+  useEffect(() => {
+    document.body.style.backgroundColor = 'white';
+  }, []);
+  
   return (
     <div className="container">
       <h2>React 색상 선택</h2>
-      <hr />
+  
+      {colors.map((color) => (
+        <div
+          key={color}
+          className="color-box"
+          style={{ backgroundColor: color }}
+          onClick={() => navigate(`/color/${color}`)}
+        >
+          {color}
+        </div>
+      ))}
+  
+      <hr /> 
     </div>
   );
+  
+
+  
 };
 
 export default Mainpage;
+
+
+
